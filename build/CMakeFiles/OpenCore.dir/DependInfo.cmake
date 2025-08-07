868966b2295ecfd9361bd114c3de7533678f5ce4
Monsieur Doceo <plasteiggregory@gmail.com>
2025-08-07 16:55:07 +0200

    First startup

---
+++ build/CMakeFiles/OpenCore.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/doceo/Core/Code/C++/OpenCore/src/main.cpp" "CMakeFiles/OpenCore.dir/src/main.cpp.o" "gcc" "CMakeFiles/OpenCore.dir/src/main.cpp.o.d"
+  "" "OpenCore" "gcc" "CMakeFiles/OpenCore.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/OpenCore.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenCore.dir/link.d"
+  "CMakeFiles/OpenCore.dir/src/main.cpp.o"
+  "CMakeFiles/OpenCore.dir/src/main.cpp.o.d"
+  "OpenCore"
+  "OpenCore.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenCore.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
