e3720a228963a95685957962c8d948a4d9650dff
Monsieur Doceo <plasteiggregory@gmail.com>
2025-08-23 23:33:56 +0200

    Added a little documentation.

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,7 @@ void processInput(GLFWwindow* window)
 
 int main()
 {
+	// Initialising glfw
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -35,6 +36,7 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
+	// Creating a window
 	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenCore", NULL, NULL);
 	if (window == NULL)
 	{
@@ -43,6 +45,7 @@ int main()
 		return -1;
 	}
 
+	// Init GLad and create a context to the window
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
@@ -51,6 +54,7 @@ int main()
 		return -1;
 	}
 
+	// Creating a shader
 	Shader shader("../res/shaders/vertex.vs", "../res/shaders/fragment.fs");
 
 	float vertices[] = {
@@ -63,48 +67,64 @@ int main()
 		0, 1, 3,
 		1, 2, 3
 	};
+
+	// Creating a VBO[Vertex Buffer Object], a VAO[Vertex Array Object] and a EBO[Element Buffer Object]
 	unsigned int VBO, VAO, EBO;
+	// Asign the VAO, VBO and EBO
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
 	glGenBuffers(1, &EBO);
 	glBindVertexArray(VAO);
 
+	// Binding the VBO buffer to the GPU
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	// Binding the EBO buffer to the GPU
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	
+	// Asign and creating a array pointer to the vertices
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*) 0);
 	glEnableVertexAttribArray(0);
 
+	// Asign and creating a array pointer to the color
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*) (3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
+	// Asign and creating a array pointer to the texture position
 	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*) (6 * sizeof(float)));
         glEnableVertexAttribArray(2);	
 
+	// Unbind the VBO and VAO
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
-	
+
+	// Creating textures 
 	Texture texture1("../res/textures/wall.jpg", false, false);
 	Texture texture2("../res/textures/awesomeface.png", true, true);
 	
 	shader.use();
 
+	// Asign textures to the GPU to draw
 	shader.setInt("texture1", 0);
 	shader.setInt("texture2", 1);
 
+	// Run till the window close
 	while (!glfwWindowShouldClose(window))
 	{
+		// Register Input
 		processInput(window);
 
+		// Clear the screen
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		// Asign the texture order
 		texture1.use(GL_TEXTURE0);
 		texture2.use(GL_TEXTURE1);
 
+		// Use GLM to make mathematical calculation to rotate the texture
 		glm::mat4 trans = glm::mat4(1.0f);
 		trans = glm::scale(trans, glm::vec3(0.5f, 0.5f, 0.5f));
 		trans = glm::translate(trans, glm::vec3(0.6f, -0.6f, 0.0f));
@@ -113,6 +133,7 @@ int main()
 		unsigned int transformLoc = glGetUniformLocation(shader.getID(), "transform");
 		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
+		// Draw the element
 		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
@@ -136,10 +157,12 @@ int main()
 
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
+		// Get all events and register them on the window
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
+	// Free the ram and useless buffer and shader
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
 	glDeleteBuffers(1, &EBO);
