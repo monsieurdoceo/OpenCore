471e1a220e2b3ba65288c1986c17a4ae816a2919
Monsieur Doceo <plasteiggregory@gmail.com>
2025-11-04 10:02:37 +0100

    Added and calculated ambient and diffuse light in the vertex and fragment shaders.

--- res/shaders/lightVertex.vs
+++ res/shaders/lightVertex.vs
@@ -9,3 +9,4 @@ void main()
 {
 	gl_Position = projection * view * model * vec4(aPos, 1.0);
 }
+
--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+Camera camera(glm::vec3(0.0f, 1.0f, 3.0f));
 float lastX = 800.0f / 2.0f;
 float lastY = 600.0f / 2.0f;
 bool firstMouse = true;
@@ -113,47 +113,47 @@ int main()
 	Shader lightSourceShader("../res/shaders/lightVertex.vs", "../res/shaders/lightFragment.fs");
 
 	float vertices[] = {
-    		-0.5f, -0.5f, -0.5f,
-     		 0.5f, -0.5f, -0.5f,
-     	    	 0.5f,  0.5f, -0.5f,
-     	 	 0.5f,  0.5f, -0.5f,
-    		-0.5f,  0.5f, -0.5f,
-    		-0.5f, -0.5f, -0.5f,
+    		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+     		 0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 
+     		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 
+     		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 
+    		-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 
+    		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 
 
-    		-0.5f, -0.5f,  0.5f,
-     		 0.5f, -0.5f,  0.5f,
-     		 0.5f,  0.5f,  0.5f,
-     		 0.5f,  0.5f,  0.5f,
-    		-0.5f,  0.5f,  0.5f, 
-    		-0.5f, -0.5f,  0.5f,
+    		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+     		 0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+     		 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+     		 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+    		-0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+    		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
 
-	       	-0.5f,  0.5f,  0.5f,
-	       	-0.5f,  0.5f, -0.5f,	
-       		-0.5f, -0.5f, -0.5f,
-		-0.5f, -0.5f, -0.5f,
-		-0.5f, -0.5f,  0.5f,
-		-0.5f,  0.5f,  0.5f,
+    		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+    		-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+    		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+    		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+    		-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+    		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
 
- 		 0.5f,  0.5f,  0.5f,
-   		 0.5f,  0.5f, -0.5f,
-   		 0.5f, -0.5f, -0.5f,
-   		 0.5f, -0.5f, -0.5f,
-     		 0.5f, -0.5f,  0.5f,
-     		 0.5f,  0.5f,  0.5f,
+    		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+    		 0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+    		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+    		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+    		 0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+    		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
 
-   		-0.5f, -0.5f, -0.5f,
-     		 0.5f, -0.5f, -0.5f,
-     		 0.5f, -0.5f,  0.5f,
-     		 0.5f, -0.5f,  0.5f, 
-    		-0.5f, -0.5f,  0.5f,
-    		-0.5f, -0.5f, -0.5f,
+    		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+    		 0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+    		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+    		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+    		-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+    		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
 
-    		-0.5f,  0.5f, -0.5f,
- 		 0.5f,  0.5f, -0.5f,
-     		 0.5f,  0.5f,  0.5f,
-     		 0.5f,  0.5f,  0.5f,
-    		-0.5f,  0.5f,  0.5f,
-    		-0.5f,  0.5f, -0.5f
+    		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+    		 0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+    		 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+    		 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+    		-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+    		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
 	};
 
 	// Creating a VBO[Vertex Buffer Object], a VAO[Vertex Array Object] and a EBO[Element Buffer Object]
@@ -168,14 +168,17 @@ int main()
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	
 	// Asign and creating a array pointer to the vertices
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
-	glEnableVertexAttribArray(0);	
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) 0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) (3 * sizeof(float)));
+	glEnableVertexAttribArray(1);	
 
 	unsigned int lightCubeVAO;
 	glGenVertexArrays(1, &lightCubeVAO);
 	glBindVertexArray(lightCubeVAO);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) 0);
 	glEnableVertexAttribArray(0);
 
 	// Unbind the VBO and VAO
@@ -200,6 +203,7 @@ int main()
 		shader.use();
 		shader.setVec3("objectColor", glm::vec3(1.0f, 0.5f, 0.31f));
 		shader.setVec3("lightColor", glm::vec3(1.0f, 1.0f, 1.0f));
+		shader.setVec3("lightPos", lightPos);
 
 		glm::mat4 projection = glm::perspective(glm::radians(camera.getZoom()), 800.0f / 600.0f, 0.1f, 100.0f);
 		shader.setMat4("projection", projection);
@@ -220,7 +224,7 @@ int main()
 		
 		model = glm::mat4(1.0f);
 		model = glm::translate(model, lightPos);
-		model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
+		model = glm::scale(model, glm::vec3(0.6f)); // a smaller cube
 		lightSourceShader.setMat4("model", model);
 
 		glBindVertexArray(lightCubeVAO);
